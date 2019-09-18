17ce8980d8080aec0b3a55cbbc810fa8789f4067
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-09-18 13:53:40 +0200

    Finished task C

--- src/jon-mikkel_ex/ex01/letters_counts.py
+++ src/jon-mikkel_ex/ex01/letters_counts.py
@@ -1,6 +1,14 @@
+import collections
+
 def letter_freq(txt):
-    """Creates a """
-    dict_for_letters: {}
+    """Creates a dictionary from a string, by lowering all characters, then going through each character one by one.
+        Checks if its new to start counting that character,
+        Returns
+        -------
+        An alphabetically sorted dictionary by using the OrderedDict from Collections
+        Link - https://stackoverflow.com/questions/9001509/how-can-i-sort-a-dictionary-by-key
+    """
+    dict_for_letters = {}
     txt.lower()
     for each_character in txt:
         if each_character in dict_for_letters:
@@ -8,12 +16,13 @@ def letter_freq(txt):
         else:
             dict_for_letters[each_character] = 1
 
-    return dict_for_letters
+    return collections.OrderedDict(sorted(dict_for_letters.items()))
 
 
 if __name__ == '__main__':
     text = input('Please enter text to analyse: ')
 
     frequencies = letter_freq(text)
+    print(frequencies)
     for letter, count in frequencies.items():
         print('{:3}{:10}'.format(letter, count))

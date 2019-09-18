6b30fd8ff8f73f62ac1f8949d2d63e8a532c05e8
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-09-18 07:30:15 +0200

    Made changes to the Card_game after lecture 2

--- src/jon-mikkel_ex/card-game.py
+++ src/jon-mikkel_ex/card-game.py
@@ -1,53 +1,72 @@
-import random
+from random import shuffle
 
-def number_of_cards():
-    number_of_card = []
-    for number in range(1, 14):
-        number_of_card.append(number)
 
-    return number_of_card
+def play_game():
+    """ Play a single game of modified clock patience.
+    Returns
+    -------
+    win: bool
+        True if the game resulted in a win and False otherwise
+    """
+    cards = create_deck_of_cards()
+    shuffle(cards)
+    board = create_board()
+
+    while not game_finished(cards, board):
+        cards, board = play_single_round(cards, board)
+
+    return is_winning_state(board)
 
 
 def create_deck_of_cards():
-    deck =[]
-    suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
-    for suit in suits:
-        deck.append((suit, number_of_cards()))
+    """ Returns a deck of cards represented by a list of 2-tuples.
 
-    return deck
+    Each tuple represents one card, the first element is either
+    ``'C'``, ``'D'``, ``'H'``, ``'S'``, representing clubs, diamonds, hearts and spades,
+    The  second element represent the value of the card. 1 is ace, 11 is jack, 12, is queen, and 13 is king
+    """
+    suits = ['C', 'D', 'H', 'S']
+    return [(suit, value) for suit in suits for value in range(1, 14)]
 
-def shuffle_deck():
-    return random.shuffle(create_deck_of_cards())
 
 def create_board():
-    board = {}
-    for position in range(1,14):
-        board[position] = None
-
-    return board
-
-def play_single_round(board):
-    pass
+    """ Create an emptt board with 13 2-tuples containing ``None`` values
+    """
+    return [(None, None) for _ in range(13)]
 
 
-
-def check_winning_board():
-    pass
-
-
-print(create_deck_of_cards())
-
-class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
-
-    def show(self):
-        print("{} of {}".format(self.value, self.suit))
+def game_finished(cards, board):
+    """ Returns True if the game is over, False otherwise,
+    """
+    return is_winning_state(board) or (len(cards) == 0)
 
 
+def is_winning_state(board):
+    for position in range(13):
+        if board[position][1] != position + 1:
+            return False
+
+    return True
 
 
+def play_single_round(cards, board):
+    """ Play a single round of clock patience
+
+    Returns
+    -------
+    cards: list
+        Deck of cards after playing a round
+    board: list
+        Board state after playing a round
+    """
+    for position, board_state in enumerate(board):
+        if board_state == position + 1:
+            continue
+        if len(cards) == 0:
+            break
+        board[position] = cards.pop()
+
+    return cards, board
 
 
 

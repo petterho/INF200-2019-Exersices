a4a14e1073732558f0e9559df3b4a320666e6209
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-09-23 15:06:23 +0200

    Finished task A in exercise 02

--- src/jon-mikkel_ex/ex02/file_stats.py
+++ src/jon-mikkel_ex/ex02/file_stats.py
@@ -1,5 +1,20 @@
 def char_counts(textfilename):
-    pass
+    """Opens a file for reading and makes a list of each character with a code lower than 256, and records the
+        frequencies
+        Returns
+        --------
+        A list of each charactercode and its frequency
+    """
+
+    list_of_charcode_with_freq = [0 for _ in range(256)]
+    with open(textfilename, 'r', encoding='utf-8') as txt:
+        for line in txt:
+            for char in line:
+                if ord(char) < 256:
+                 list_of_charcode_with_freq[ord(char)] += 1
+
+    return list_of_charcode_with_freq
+
 
 if __name__ == '__main__':
 

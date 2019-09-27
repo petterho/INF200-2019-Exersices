b3ea4c8a5aa28fce72b749752a14826b171c7950
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-09-27 10:41:10 +0200

    Install numpy, updated Task A for testing bubble_sort

--- src/jon_mikkel_ex/ex03_project/test_sorting.py
+++ src/jon_mikkel_ex/ex03_project/test_sorting.py
@@ -4,33 +4,29 @@ __author__ = "Jon-Mikkel Korsvik"
 __email__ = "jonkors@nmbu.no"
 
 from src.jon_mikkel_ex.ex02.bubble_sort import bubble_sort
+import random
+import numpy as np
 
 
 def test_empty():
     """Test that the sorting function works for empty list"""
-
-
-pass
+    assert bubble_sort([]) == []
 
 
 def test_single():
     """Test that the sorting function works for single-element list"""
-    pass
+    assert bubble_sort([1]) == [1]
 
 
 def test_sorted_is_not_original():
     """
     Test that the sorting function returns a new object.
 
-    Consider
-
-    data = [3, 2, 1]
-    sorted_data = bubble_sort(data)
-
     Now sorted_data shall be a different object than data,
     not just another name for the same object.
     """
-    pass
+    data = [3, 2, 1]
+    assert bubble_sort(data) is not data
 
 
 def test_original_unchanged():
@@ -39,27 +35,30 @@ def test_original_unchanged():
 
     Consider
 
-    data = [3, 2, 1]
+    data =
     sorted_data = bubble_sort(data)
 
     Now data shall still contain [3, 2, 1].
     """
+    data = [3, 2, 1]
+    bubble_sort(data)
+    assert data == [3, 2, 1]
     pass
 
 
 def test_sort_sorted():
     """Test that sorting works on sorted data."""
-    pass
+    assert bubble_sort([1, 2, 3]) == [1, 2, 3]
 
 
 def test_sort_reversed():
     """Test that sorting works on reverse-sorted data."""
-    pass
+    assert bubble_sort([3, 2, 1]) == [1, 2, 3]
 
 
 def test_sort_all_equal():
     """Test that sorting handles data with identical elements."""
-    pass
+    assert bubble_sort([1, 5, 3, 2, 3, 4, 1]) == [1, 1, 2, 3, 3, 4, 5]
 
 
 def test_sorting():
@@ -70,4 +69,32 @@ def test_sorting():
     ensure that they are sorted correctly. These could be lists of
     numbers of different length or lists of strings.
     """
-    pass
+    data = []
+    for i in range(1000):  # Testing integers
+        data.append(random.randint(1, 1000))
+
+    new_data = bubble_sort(data)
+
+    for index in range(len(new_data)-1):
+        assert new_data[index] <= new_data[index+1]
+
+    data = np.random.random(100)  # Testing floats
+    new_data = bubble_sort(data)
+
+    for index in range(len(new_data)-1):
+        assert new_data[index] <= new_data[index+1]
+
+    list_of_strings = ('aaa', 'bb', 'ii', 'qwr')
+    bubble_sort(list_of_strings)
+
+
+
+if __name__ == "__main__":
+    test_empty()
+    test_original_unchanged()
+    test_single()
+    test_sort_all_equal()
+    test_sort_reversed()
+    test_sort_sorted()
+    test_sorted_is_not_original()
+    test_sorting()

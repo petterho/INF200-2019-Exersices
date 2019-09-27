f391bee9408609f49a0b53f3d396ac640a9770f8
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-09-27 09:18:16 +0200

    Started task B

---
+++ src/jon_mikkel_ex/ex03_project/test_median.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Jon-Mikkel Korsvik"
+__email__ = "jonkors@nmbu.no"
+
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))

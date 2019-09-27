014978ecce5d3193f373d3b76f809c31900659e6
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-09-27 11:44:37 +0200

    Added test for odd number of elements
    Successful

--- src/jon_mikkel_ex/ex03_project/test_median.py
+++ src/jon_mikkel_ex/ex03_project/test_median.py
@@ -22,6 +22,11 @@ def test_single_element_list():
     assert median([4]) == 4
 
 
+def test_list_odd_number_of_elements():
+    assert median([1, 2, 3, 4, 5]) == 3
+
+
 if __name__ == "__main__":
     test_single_element_list()
+    test_list_odd_number_of_elements()
 

f3f04136e44bccb95013b02d0c315e20c6ec191e
Petter Bøe Hørtvedt <petterho@nmbu.no>
2019-10-22 12:47:15 +0200

    Made the ListRand class

--- src/petter_bøe_hørtvedt_ex/ex04/myrand.py
+++ src/petter_bøe_hørtvedt_ex/ex04/myrand.py
@@ -3,3 +3,23 @@
 __author__ = 'Petter Bøe Hørtvedt'
 __email__ = 'petterho@nmbu.no'
 
+
+class ListRand:
+    def __init__(self, number_list):
+        self.list = number_list
+        self.index = 0
+
+    def rand(self):
+        if self.index == len(self.list):
+            raise RuntimeError('')
+
+        number = self.list[self.index]
+        self.index += 1
+        return number
+
+
+if __name__ == '__main__':
+    listrand_instance = ListRand([1, 2, 3])
+    listrand_instance.rand()
+    listrand_instance.rand()
+    listrand_instance.rand()

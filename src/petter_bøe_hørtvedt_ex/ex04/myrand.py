5774be441de33a3ec54418302b91ac5295d45b06
Petter Bøe Hørtvedt <petterho@nmbu.no>
2019-10-22 12:20:54 +0200

    Started the task

---
+++ src/petter_bøe_hørtvedt_ex/ex04/myrand.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Petter Bøe Hørtvedt'
+__email__ = 'petterho@nmbu.no'
+
---
+++ src/petter_bøe_hørtvedt_ex/ex04/walker.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Petter Bøe Hørtvedt'
+__email__ = 'petterho@nmbu.no'

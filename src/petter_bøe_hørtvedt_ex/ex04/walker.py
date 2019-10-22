df3072dbb83caf6516f7614d861f4ececb314231
Petter Bøe Hørtvedt <petterho@nmbu.no>
2019-10-22 13:44:25 +0200

    Started writing the walker class

--- src/petter_bøe_hørtvedt_ex/ex04/walker.py
+++ src/petter_bøe_hørtvedt_ex/ex04/walker.py
@@ -2,3 +2,24 @@
 
 __author__ = 'Petter Bøe Hørtvedt'
 __email__ = 'petterho@nmbu.no'
+
+
+import random
+
+
+class Walker:
+    def __init__(self, start_position, home_position):
+        self.start_position = start_position
+        self.home_position = home_position
+
+    def move(self):
+        raise NotImplementedError
+
+    def is_at_home(self):
+        raise NotImplementedError
+
+    def get_position(self):
+        raise NotImplementedError
+
+    def get_steps(self):
+        raise NotImplementedError

b17b5b4d05e8529682d2f1a0c536dbafc367e1d0
Petter Bøe Hørtvedt <petterho@nmbu.no>
2019-10-22 15:25:49 +0200

    Updated class walker and made the main part

--- src/petter_bøe_hørtvedt_ex/ex04/walker.py
+++ src/petter_bøe_hørtvedt_ex/ex04/walker.py
@@ -9,17 +9,39 @@ import random
 
 class Walker:
     def __init__(self, start_position, home_position):
-        self.start_position = start_position
+        self.position = start_position
         self.home_position = home_position
+        self.steps = 0
 
     def move(self):
-        raise NotImplementedError
+        step = -1 if random.randint(0, 2) == 0 else 1
+        self.position += step
+        self.steps += 1
+        return step
 
     def is_at_home(self):
-        raise NotImplementedError
+        if self.position == self.home_position:
+            return True
+        else:
+            return False
 
     def get_position(self):
-        raise NotImplementedError
+        return self.position
 
     def get_steps(self):
-        raise NotImplementedError
+        return self.steps
+
+    def walk_home(self):
+        while not self.is_at_home():
+            self.move()
+        return self.steps
+
+
+if __name__ == '__main__':
+    distances_home = [1, 2, 5, 10, 20, 50, 100]
+    for distance in distances_home:
+        steps_list = []
+        for students in range(5):
+            student = Walker(0, distance)
+            steps_list.append(student.walk_home())
+        print(f'Distance:{distance:4} Path lengths: {steps_list}')

71674700439e744a3457e8819e6317d32abda354
Petter Bøe Hørtvedt <petterho@nmbu.no>
2019-10-23 16:42:39 +0200

    Making the files

---
+++ src/petter_bøe_hørtvedt_ex/ex05/bounded_sim.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Petter Bøe Hørtvedt'
+__email__ = 'petterho@nmbu.no'
+
+
+from . walker_sim import Walker, Simulation
+
+
+class BoundedWalker:
+    def __init__(self, start, home, left_limit, right_limit):
+        """
+        Initialise the walker
+
+        Arguments
+        ---------
+        start : int
+            The walker's initial position
+        home : int
+            The walk ends when the walker reaches home
+        left_limit : int
+            The left boundary of walker movement
+        right_limit : int
+            The right boundary  of walker movement
+        """
+
+
+class BoundedSimulation:
+    def __init__(self, start, home, seed, left_limit, right_limit):
+        """
+        Initialise the simulation
+
+        Arguments
+        ---------
+        start : int
+            The walker's initial position
+        home : int
+            The walk ends when the walker reaches home
+        seed : int
+            Random generator seed
+        left_limit : int
+            The left boundary of walker movement
+        right_limit : int
+            The right boundary  of walker movement
+        """
--- src/petter_bøe_hørtvedt_ex/ex05/walker_sim.py
+++ src/petter_bøe_hørtvedt_ex/ex05/walker_sim.py
@@ -44,7 +44,54 @@ class Walker:
         return self.steps
 
     def walk_home(self):
+        """
+        A method that automatically walks the student home.
+
+        Returns
+        -------
+        self.steps: int
+            Number of steps taken to get home.
+        """
         while not self.is_at_home():
             self.move()
         return self.steps
 
+class Simulation:
+    def __init__(self, start, home, seed):
+        """
+        Initialise the simulation
+
+        Arguments
+        ---------
+        start : int
+            The walker's initial position
+        home : int
+            The walk ends when the walker reaches home
+        seed : int
+            Random generator seed
+        """
+
+    def single_walk(self):
+        """
+        Simulate single walk from start to home, returning number of steps.
+
+        Returns
+        -------
+        int
+            The number of steps taken
+        """
+
+    def run_simulation(self, num_walks):
+        """
+        Run a set of walks, returns list of number of steps taken.
+
+        Arguments
+        ---------
+        num_walks : int
+            The number of walks to simulate
+
+        Returns
+        -------
+        list[int]
+            List with the number of steps per walk
+        """

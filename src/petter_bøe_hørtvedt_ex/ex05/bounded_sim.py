4c3d4a091419896dbb7a64784ff8e5aa268c78b3
Petter Bøe Hørtvedt <petterho@nmbu.no>
2019-10-24 15:12:13 +0200

    Made the new classes a subclass of the superclass

--- src/petter_bøe_hørtvedt_ex/ex05/bounded_sim.py
+++ src/petter_bøe_hørtvedt_ex/ex05/bounded_sim.py
@@ -4,11 +4,10 @@ __author__ = 'Petter Bøe Hørtvedt'
 __email__ = 'petterho@nmbu.no'
 
 
-from . walker_sim import Walker, Simulation
-import random
+from src.petter_bøe_hørtvedt_ex.ex05.walker_sim import Walker, Simulation
 
 
-class BoundedWalker:
+class BoundedWalker(Walker):
     def __init__(self, start, home, left_limit, right_limit):
         """
         Initialise the walker
@@ -26,14 +25,17 @@ class BoundedWalker:
         """
         self.left_limit = left_limit
         self.right_limit = right_limit
+        super().__init__(start, home)
 
-    class Walker(BoundedWalker):
+    def move(self):
+        Walker.move(self)
+        if self.position < self.left_limit:
+            self.position = self.left_limit
+        if self.position > self.right_limit:
+            self.position = self.right_limit
 
 
-    
-
-
-class BoundedSimulation:
+class BoundedSimulation(Simulation):
     def __init__(self, start, home, left_limit, right_limit, seed=None):
         """
         Initialise the simulation
@@ -51,11 +53,32 @@ class BoundedSimulation:
         seed : int
             Random generator seed
         """
-        self.start = start
-        self.home = home
         self.left_limit = left_limit
         self.right_limit = right_limit
-        if seed is None:
-            pass
-        else:
-            random.seed(seed)
+        super().__init__(start, home, seed)
+
+    def single_walk(self):
+        """
+        Simulate single walk from start to home, returning number of steps.
+
+        Returns
+        -------
+        int
+            The number of steps taken
+        """
+        student = BoundedWalker(
+            self.start, self.home, self.left_limit, self.right_limit)
+        return student.walk_home()
+
+
+if __name__ == '__main__':
+    start_position = 0
+    home_position = 20
+    left_boundarys = [0, -10, -100, -1000, -10000]
+    right_boundary = 20
+    walks = 20
+    for left_boundary in left_boundarys:
+        print(f'Left boundary: {left_boundary:8}')
+        sim_ist = BoundedSimulation(start_position, home_position,
+                                    left_boundary, right_boundary)
+        print(sim_ist.run_simulation(walks))

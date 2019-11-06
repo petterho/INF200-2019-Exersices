46fab29517e532ca56a140a32b714d645da13c1f
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-11-06 13:34:16 +0100

    Working on Board Class

--- src/pa02/snakes_simulations.py
+++ src/pa02/snakes_simulations.py
@@ -8,7 +8,7 @@ __version__ = "0.0.1"
 class Board:
     """
     Handles the information about the board, including ladders, snakes,
-    end and start position.
+    goal.
 
     Methods:
         constructor
@@ -17,14 +17,77 @@ class Board:
 
         position_adjustment
     """
+    def __init__(self, ladders=None, chutes=None, goal=None):
+        if ladders is not None:
+            self.ladders = ladders
+        else:
+            self.ladders = [(1, 40), (8, 10), (36, 52), (43, 62),
+                            (49, 79), (65, 82), (68, 85)]
+        if chutes is not None:
+            self.chutes = chutes
+        else:
+            self.chutes = [(24, 5), (33, 3), (42, 30), (56, 37),
+                           (64, 27), (74, 12), (87, 70)]
+        if goal is not None:
+            self.goal = goal
+        else:
+            self.goal = 90
+
+
+    def goal_reached(self, position):
+        return position >= self.goal
+
+
+    def position_adjustment(self, position):
+        if position in self.ladders[:][0] or self.chutes[:][0]:
+            return self.ladder[self.ladder.index(position)]
+
+
+class Player:
+    """
+    __init___
+    input - instance of Board
+
+    Methods:
+        move() - Moves player and checks with board position
+
+    """
     def __init__(self):
-        pass
 
-    def constructor(self):
-        pass
 
-    def goal_reached(self):
-        pass
+class LazyPlayer(Player):
+    """
+    Subclass of player
+    """
 
-    def position_adjustment(self):
-        pass
+    def __init__(self):
+        super().__init__()
+
+
+class ResilientPlayer(Player):
+    """
+    Subclass of player
+    """
+    def __init__(self):
+        super().__init__()
+
+
+class Simulation:
+    """
+    Manages simulation of the
+    """
+
+    def singe_game(self):
+
+    def run_simulation(self):
+
+    def get_results(self):
+
+    def winners_per_type(self):
+
+    def durations_per_type(self):
+
+    def players_per_type(self):
+
+
+

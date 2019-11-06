28098d851eaeef794151f7024a7a8fc56d0d3ade
Petter Bøe Hørtvedt <petterho@nmbu.no>
2019-11-06 12:59:37 +0100

    Started the task

--- src/pa02/snakes.py
+++ src/pa02/snakes.py
@@ -1,3 +1,6 @@
-# First comment
-# Second comment
-# Third comment
+# -*- coding: utf-8 -*-
+
+__author__ = "Jon-Mikkel Korsvik", "Petter Bøe Hørtvedt"
+__email__ = "jonkors@nmbu.no", "petterho@nmbu.no"
+__version__ = "0.0.1"
+
---
+++ src/pa02/snakes_simulations.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Jon-Mikkel Korsvik", "Petter Bøe Hørtvedt"
+__email__ = "jonkors@nmbu.no", "petterho@nmbu.no"
+__version__ = "0.0.1"
+
+
+class Board:
+    """
+    Handles the information about the board, including ladders, snakes,
+    end and start position.
+
+    Methods:
+        constructor
+
+        goal_reached
+
+        position_adjustment
+    """
+    def __init__(self):
+        pass
+
+    def constructor(self):
+        pass
+
+    def goal_reached(self):
+        pass
+
+    def position_adjustment(self):
+        pass
